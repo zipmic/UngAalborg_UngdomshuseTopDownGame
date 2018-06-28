06715c551ee10ff2ead2e2a79fe82459da7b8a78
Christian Skriver Kragegaard <mail@code.dk>
2018-06-28 15:42:53 +0200

    intial

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        //   offset = transform.position - player.transform.position;
+        offset = new Vector3(0, 8, -10);
+    }
+    void Update()
+    {
+        
+    }
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = player.transform.position + offset;
+       // if (Input.GetKey(KeyCode.E))
+       // {
+       //     transform.Rotate(5.0f, 0.0f, 0.0f);
+      //  }
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.RotateAround(player.transform.position, 50*Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Camera MainCamera;
+    private Rigidbody rb;
+    public GameObject PlayerGraphic;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        Vector3 targetDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            targetDirection = (transform.position-Camera.main.transform.position);
+            targetDirection.y = 0;
+            targetDirection.Normalize();
+            //targetDirection = Camera.main.transform.TransformDirection(Vector3.forward).normalized;
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+          
+            targetDirection = Camera.main.transform.right*-1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            targetDirection = Camera.main.transform.right;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            targetDirection = (transform.position - Camera.main.transform.position)*-1;
+            targetDirection.y = 0;
+            targetDirection.Normalize();
+       
+        }
+
+        targetDirection.y = rb.velocity.y;
+        rb.velocity = targetDirection * Time.deltaTime * speed;
+
+
+        if(targetDirection != Vector3.zero)
+        {
+            PlayerGraphic.transform.rotation = Quaternion.Slerp(PlayerGraphic.transform.rotation, Quaternion.LookRotation(targetDirection), 0.15F);
+        }
+
+
+
+
+        //print(targetDirection);
+
+     
+       
+
+  
+    }
+        
+}
